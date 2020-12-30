0e6f949c0ca70acf342ae08014a51f0b4068a38f
janmaijay1997 <30903817+janmaijay1997@users.noreply.github.com>
2020-12-30 13:18:30 +0530

    java 8 changes

--- LambdaExpressionExample/src/com/lambdaExpressionExample/LambdaExpression.java
+++ LambdaExpressionExample/src/com/lambdaExpressionExample/LambdaExpression.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.concurrent.locks.Condition;
+import java.util.stream.Collectors;
 
 public class LambdaExpression {
 	
@@ -18,7 +20,7 @@ public class LambdaExpression {
 		
 		//sorting based on last name
 		//old fashion java 7
-		Collections.sort(personList, new Comparator<Person>() {
+		/*Collections.sort(personList, new Comparator<Person>() {
 
 			@Override
 			public int compare(Person o1, Person o2) {
@@ -26,7 +28,7 @@ public class LambdaExpression {
 				return o2.getLastName().compareToIgnoreCase(o1.getLastName());
 			}
 			
-		});
+		});*/
 		
 		for(Person person:personList) {
 			//System.out.println(person);
@@ -37,9 +39,41 @@ public class LambdaExpression {
 			System.out.println(person);
 			}
 		}
+		 //sorting by last name using java8
+		
+		Collections.sort(personList,(o1,o2)->
+			o1.getLastName().compareToIgnoreCase(o2.getLastName())
+		);
+		
+		System.out.println("java 8 result ");
+		for(Person person:personList) {
+			System.out.println(person);
+		}
+		
+		//last name contain b or not in java 8 version
+
+		System.out.println("java 8 result start with b  ");
+		for(Person person:personList) {
+		printCondtinally(person, p->p.getLastName().toUpperCase().startsWith("B"));
+		}
+		//from java 8 using stream
+		
+		//personList.stream().sorted(Collections.so)
+		
+	}
+
+	private static void printCondtinally(Person person, Condition condition) {
+
+		if(condition.test(person)) {
+			System.out.println(person);
+		}
 		
 	}
 	
 	
+	
+	interface Condition{
+		public boolean test(Person p);
+	}
 
 }
